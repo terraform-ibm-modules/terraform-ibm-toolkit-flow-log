40fbf05f7c1456998bcf69e01b4f9e78c3bd4afe
vamsysivaarcca26 <106111117+vamsysivaarcca26@users.noreply.github.com>
2022-08-09 12:09:59 -0700

    Updated workflows to match current templates (#4)
    * Updated workflows to match current templates
    
    Closes #3
    
    Signed-off-by: Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>
    
    Co-authored-by: Vamsy Sivaarcca <vamsysivaarcca@Vamsys-MacBook-Pro.local>

--- test/stages/stage1-resource-group.tf
+++ test/stages/stage1-resource-group.tf
@@ -1,6 +1,6 @@
 module "resource_group" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-resource-group.git"
-
+  ibmcloud_api_key = var.ibmcloud_api_key
   resource_group_name = var.resource_group_name
-  provision           = false
+  
 }
