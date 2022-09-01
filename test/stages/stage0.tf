043fb3d9b10953af7aeec3c6d26ad4b4af483a8a
Bala Sivasubramanian <bala@us.ibm.com>
2022-09-01 14:47:41 -0500

    update README/CN Signed-off-by: Bala <@balasgit> (#5)
    * update README/CN Signed-off-by: Bala <@balasgit>
    
    * fix compile errors
    
    * updated the container image
    
    * trying with ibmcloud CLI

--- test/stages/stage0.tf
+++ test/stages/stage0.tf
@@ -8,3 +8,16 @@ terraform {
     }
   }
 }
+
+module setup_test_clis {
+  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
+
+  bin_dir = "${path.cwd}/test_bin_dir"
+  clis = ["kubectl", "oc"]
+}
+
+resource local_file bin_dir {
+  filename = "${path.cwd}/.bin_dir"
+
+  content = module.setup_test_clis.bin_dir
+}
